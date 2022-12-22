4ce684041896167e3596f7f7fa62f52b29fa3eb3
lasttrader <grishaperm@list.ru>
2022-12-22 17:58:31 +0300

    add bootstrap for pages  template

--- project/project/settings.py
+++ project/project/settings.py
@@ -129,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
