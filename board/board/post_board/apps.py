ae2ebea64d3dba4da016c9e5f578654f734a471b
lasttrader <grishaperm@list.ru>
2023-04-27 16:46:26 +0300

    added small html example

---
+++ board/board/post_board/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PostBoardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'post_board'
---
+++ board/board/post_board/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+from django.shortcuts import reverse
+
+# Create your models here.
